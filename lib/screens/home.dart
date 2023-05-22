b39d1a3feed64a57a944cd83895f4817f9667dc8
Merve Ağaçayak <mrve.agcyk@gmail.com>
2023-05-22 15:04:10 +0300

    creating search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,9 +11,37 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGcolor,
       appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is home screen.'),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget searchBox() {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: const TextField(
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(
+                Icons.search,
+                color: tdBlack,
+                size: 20,
+              ),
+              prefixIconConstraints: BoxConstraints(
+                maxHeight: 20,
+                minWidth: 25,
+              ),
+              border: InputBorder.none,
+              hintText: 'Search',
+              hintStyle: TextStyle(color: tdGrey)),
+        ));
   }
 
   AppBar _buildAppBar() {
