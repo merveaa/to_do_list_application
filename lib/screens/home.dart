f29a1136507f0de74258e42f077024b9d9f9c20f
Merve Ağaçayak <mrve.agcyk@gmail.com>
2023-05-22 13:49:12 +0300

    creating project structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDo App'),
+      ),
+      body: Container(
+        child: Text('This is home screen.'),
+      ),
+    );
+  }
+}
